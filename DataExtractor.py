1510c0f0273f8acb23bb1f04b9c45c10ba0b13d2
Felix <qiyu_sjtu@163.com>
2020-11-26 14:29:33 +0800

    整理一下代码

--- DataExtractor.py
+++ DataExtractor.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import cv2
 import numpy
-
+from enum import Enum
 import matplotlib.pyplot as plt
 
 # 18, 121, 190
@@ -9,26 +9,31 @@ _red_lim = [10,30]
 _green_lim = [100,150]
 _blue_lim = [180,220]
 
+class AxisType(Enum):
+    LINEAR = 1
+    LOG = 2
+
 class DataExtractor:
     def __init__(self):
         self._red_lim = [0,20]
         self._green_lim = [0,20]
         self._blue_lim = [0,20]
-        self._fuzzy_range = 100 #fuzzy range refers to picking range(0~255)
+        self._fuzzy_range = 50 #fuzzy range refers to picking range(0~255)
         self._step = 10
-        self._x1 = 56
-        self._x2 = 598
-        self._y1 = 12
-        self._y2 = 612
-        self._filter_size = 2
+        self._x1 = 36
+        self._x2 = 587
+        self._y1 = 6
+        self._y2 = 423
+        self._filter_size = 3
         self.width = 0
         self.height = 0
-        self.startX = -80#从datasheet读取的数据
+        self.startX = 0.5#从datasheet读取的数据
         self.startY = 0 # 从datasheet读取的数据
-        self.endX = 160
-        self.endY = 2
+        self.endX =5
+        self.endY = 340
         self.image_width = self._x2-self._x1
         self.image_height = self._y2-self._y1
+        self.axis_type = AxisType.LINEAR
 
     def set_fuzzy_color_range(self,pixdata):
         self._red_lim[0] = pixdata[0] - self._fuzzy_range if pixdata[0]>self._fuzzy_range else 0
@@ -49,28 +54,24 @@ class DataExtractor:
 
     def filter_color_range(self,image):
         img = image
-        pixdata = img.load()
         original_image = numpy.array(img)#转换为cv2能用的格式
+        pixdata = img.load()
         cv2.imshow("original",original_image)
-        self.width = img.size[0]
-        self.height = img.size[1]
 
         for y in range(img.size[1]):
             for x in range(img.size[0]):
                 # 二值化
                 if not self.check_valid(pixdata[x,y]):
-                    pixdata[x, y] = (255,255,255,255)
+                    pixdata[x, y] = (255,255,255)
                 else:
-                    pixdata[x, y] = (0,0,0,0)
+                    pixdata[x, y] = (0,0,0)
+                    
         image = numpy.array(img)#转换为cv2能用的格式
         image = image[:, :, :].copy()
+        cv2.imshow("color_filter",image)
         
         # result是一个y*x*color_channel的数组
         result = self.remove_grid(img)
-
-        # 去除边界，但是目前这个写法可能不适合提取数据
-        # result[:,0:self._x1] = 0
-        # result[self._y2:self.height,:] = 0
 
         return result
 
@@ -87,18 +88,21 @@ class DataExtractor:
 
     def extract(self,file_name):
         img = Image.open(file_name)
-        img = img.convert("RGBA")
+        img = img.convert("RGB")
+        # self.width = img.size[0]
+        # self.height = img.size[1]
+        # 根据颜色范围选择要用到的数据
         binary_data = self.filter_color_range(img)
         # 去除边界
-        binary_data_without_border = self.remove_boarder(binary_data)
-        # cv2.imshow("border move",binary_data_without_border)
-
+        binary_data_without_border = self.remove_boarder(binary_data,[self._x1,self._x2,self._y1,self._y2])
+        # 提取数据点
         extracted_data = self.extract_data(binary_data_without_border)
+        # 绘制数据点
+        self.plot_value(extracted_data)
 
-        self.plot_value(extracted_data[:,0],extracted_data[:,1])
-
-    def remove_boarder(self,data):
-        return data[self._y1:self._y2,self._x1:self._x2]
+    def remove_boarder(self,data,filter_range):
+        # filterrange，四元向量，分别是x1,x2,y1,y2
+        return data[filter_range[2]:filter_range[3],filter_range[0]:filter_range[1]]
         
     def extract_data(self,data):
         result = []
@@ -111,12 +115,20 @@ class DataExtractor:
             if(255 in data[:,i]):
                 ldx = self.image_height-numpy.median(numpy.argwhere(data[:,i]==255))
                 result.append([i,ldx])
+
         # print(numpy.array(result))
         return numpy.array(result)
 
-    def plot_value(self,x,y):
+    def data_mapping(self,data,axis_type = AxisType.LINEAR):
+        if(self.axis_type == AxisType.LINEAR):
+            data[:,0] = data[:,0]/self.image_width*(self.endX-self.startX)+self.startX
+            data[:,1] = data[:,1]/self.image_height*(self.endY-self.startY)+self.startY
+        return data
+
+    def plot_value(self,data):
+        mapped_data = self.data_mapping(data)
         plt.figure(figsize=(5,5))
-        plt.plot(x/self.width*(self.endX-self.startX)+self.startX,y/self.height*(self.endY-self.startY)+self.startY,"-x",label="$sin(x)$",color="red",linewidth=2)
+        plt.plot(mapped_data[:,0],mapped_data[:,1],"-x",label="$sin(x)$",color="red",linewidth=2)
         plt.xlabel("x")
         plt.ylabel("y")
         plt.title("Extract Data")
@@ -128,7 +140,7 @@ class DataExtractor:
         
 
 d1 = DataExtractor()
-d1.set_fuzzy_color_range([0, 0, 0])
-d1.extract('img/4.png')
+d1.set_fuzzy_color_range([237, 34, 36])
+d1.extract('img/7.jpg')
 cv2.waitKey()
 # 780*439
